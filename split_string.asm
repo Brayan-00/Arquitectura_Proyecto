71d493a6d3c37c0a23be5198fe9c32b81d271952
Brayan-00 <brayanfernando.10@gmail.com>
2026-03-05 16:25:15 -0600

    split_string.asm is now capable to print in different lines each word in the string, beginning of a parser

--- split_string.asm
+++ split_string.asm
@@ -2,34 +2,46 @@
 ;and it splits it every time it encounters a separator (',' for example)
 
 section .data
-  input db "COLOR=3, INTERVALO=25, CARACTER=@", 0 	;test string, output should store in three registers a, b and c
+  input db "COLOR=13243, INTERVALO=251235, CARACTER=9", 0 	;test string, output should store in three registers a, b and c
   init_separator db "=",0
   fin_separator db ",", 0
   color db "Negro",0					;color puede ser 1:Rojo, 2:Verde, 3:Amarillo, 4:Azul
   intervalo db 100
   caracter db ",",0 
-
+  newline db 10
 section .bss
   charbuff resb 2
+  wordbuff resb 1024					;This buffer stores a word from the string
 section .text
 global _start
 
 _start:
   mov rsi, input 					;stores a pointer to input into rsi
-  
+  mov rdi, wordbuff					;stores a pointer to start of buffer  
+
+ 
 next_char:
   mov al, [rsi]  					;loads into an 8 bit register the first character
 
   cmp al, 0      					;stop condition: reach end of string '0'
-  je done
-
-  ;cmp al, init_separator  
-
-  ;call print_char
+  je end_word_final
 
 
-  inc rsi       					;goes to next character
-  jmp next_char
+  cmp al, [init_separator]				;checks if the character is a separator
+  je end_word
+  
+  cmp al, [fin_separator]
+  je end_word
+
+  cmp al, " "
+  je skip_char
+
+  mov [rdi], al						;store character
+  inc rdi
+  inc rsi
+  jmp next_char    
+
+ 
 
 
 done:
@@ -37,20 +49,56 @@ done:
   xor rdi, rdi
   syscall
 
-print_char:
+
+end_word_final:
+  mov byte [rdi],0
+  call print_word
+  jmp done
+
+skip_char:
+  inc rsi
+  jmp next_char
+
+end_word:
+	;This subroutine adds a 0 at the end
+  mov byte [rdi],0
+  call print_word
+
+  mov rdi, wordbuff
+
+  inc rsi
+  jmp next_char
+
+
+
+
+print_word:
 
   push rsi 						;saves the data that was on rsi
+  push rdi
+  
+  mov rdx, rdi
+  sub rdx, wordbuff					;lenght = rdi - wordbuff  
 
-  ;this prints a character
-  mov [charbuff], al 					;stores the character into memory
-  mov byte [charbuff+1], 10 				;adds \n to the end of the line
+
+
+
+;this prints a character
   mov rax, 1
   mov rdi, 1
-  mov rsi, charbuff
-  mov rdx, 2
+  mov rsi, wordbuff
   syscall
 
-  pop rsi 					;to preserve rsi
+	;print new line
+  mov rax,1
+  mov rdi,1
+  mov rsi,newline 
+  mov rdx,1
+  syscall
+
+  pop rdi
+  pop rsi 						;to preserve rsi
+   
 
   ret
 
